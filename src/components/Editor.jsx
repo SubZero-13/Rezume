d0326ace2b6bffbad26da3f6fbe12b61014d5f69
Ashish Kumar <90233808+DeveloperAshish8@users.noreply.github.com>
2024-05-07 19:39:13 +0530

    suggestion icon

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -5,6 +5,7 @@ import Projects from "./sections/Projects";
 import Skills from "./sections/Skills";
 import Education from "./sections/Education";
 import Acheivement from "./sections/Acheivements";
+import suggestion from "./suggestion.json";
 
 const Editor = ({ sections, activeIndex, setactiveIndex }) => {
   const renderContent = React.useCallback(() => {
@@ -56,8 +57,20 @@ const Editor = ({ sections, activeIndex, setactiveIndex }) => {
   }, [activeIndex]);
   return (
     <div className="flex mt-6 flex-col  md:mt-20 md:max-w-[900px] md:mx-auto mx-16 md:px-20">
-      <h1 className="font-semibold text-3xl  md:mx-0 mx-auto">
+      <h1 className="font-semibold text-3xl  md:mx-0 mx-auto flex gap-2 items-center">
         {sections[activeIndex].title}
+        <div class="group relative inline-block whitespace-nowrap">
+          <span class="flex size-6 cursor-pointer items-center justify-center rounded-full bg-primary text-sm font-semibold text-[white]">
+            i
+          </span>
+
+          <span
+            role="tooltip"
+            class="pointer-events-none absolute z-50 inline-flex items-center justify-center rounded-md px-3 py-1 font-semibold opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 bg-primary text-[white] min-h-7 text-sm left-1/2 -translate-x-1/2 after:absolute after:block after:size-0 after:border-8 after:border-transparent after:border-t-primary after:-bottom-[0.95rem] after:left-1/2 after:-translate-x-1/2 top-[-2.5rem]"
+          >
+            {suggestion[activeIndex].desc}
+          </span>
+        </div>
       </h1>
 
       {renderContent()}
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -13,7 +13,7 @@ const SideNav = ({ sections, activeIndex, setactiveIndex }) => {
   }, []);
   const [open, setOpen] = useState(width > 600 ? true : false);
   return (
-    <div className="flex absolute z-1 top-0 left-0 bg-[white]">
+    <div className="flex absolute z-1 top-0 left-0 bg-[white] ">
       <div
         className={` ${
           open ? " md:w-72 w-56" : "md:w-20 w-12"
